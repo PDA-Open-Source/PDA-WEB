e445b913b8cf8fd1f9460538c861c1451bf2119c
Snehal Gothe <92095184+snehal-gothe@users.noreply.github.com>
2022-02-15 14:50:12 +0530

    Update mobileNumberValidation.js

--- apps/authentication/static/js/custom/mobileNumberValidation.js
+++ apps/authentication/static/js/custom/mobileNumberValidation.js
@@ -97,7 +97,7 @@ $(document).ready(function () {
 //       return false;
 //     }
 //   }
-// });
+});
 
 function checkPhoneNumberLength(mobnum){
     selectedCountryCode = $("#socionUserCountryCode").val();
